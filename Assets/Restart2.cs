12e720b963a8d72f73367d522cf399c19678dde6
Fatihyusa <fatihtrkn.ft@hotmail.com>
2022-06-17 20:29:55 +0300

    Runnur is my final project

---
+++ Assets/Restart2.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Restart2 : MonoBehaviour
+{
+    public void Respwn(){
+        SceneManager.LoadScene("oyun içi ss2");
+    }
+}
---
+++ Assets/Scenes/Restart.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Restart : MonoBehaviour
+{
+    public void Restart1(){
+        SceneManager.LoadScene("oyun içi ss");
+    }
+}
